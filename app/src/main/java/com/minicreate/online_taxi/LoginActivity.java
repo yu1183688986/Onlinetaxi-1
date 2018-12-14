214197da174d11f0291b49db682f08f713982ec9
hejunweihehe <846255299@qq.com>
2018-12-14 18:37:16 +0800

    初版本代码

---
+++ app/src/main/java/com/minicreate/online_taxi/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.minicreate.online_taxi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends BaseActivity {
+    @BindView(R.id.btn_login)
+    Button mLoginBtn;
+
+    @OnClick(R.id.btn_login)
+    void login() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+}
